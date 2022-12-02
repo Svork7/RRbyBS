6fbe483bbc719674efeb508c35eee9c3d1a852c9
Eugene Mikhailov <svork@ya.ru>
2022-12-02 15:21:00 +0300

    NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './components/Home'
 import Contacts from './components/Contacts'
 import NotFound from './components/NotFound'
 import About from './components/About'
+import MainLayout from './layouts/MainLayout'
 import './App.css'
 
 function App() {
@@ -11,10 +12,12 @@ function App() {
       <div className="App">
         <Routes>
           {/* /* выводит страницу на которую нет маршрута*/}
-          <Route path="/" element={<Home />}>
-            <Route path="/about" element={<About />} />
-            <Route path="/contacts" element={<Contacts />} />
-            <Route path="/*" element={<NotFound />} />
+          <Route path="/main" element={<MainLayout />}>
+            {/*относительные ссылки*/}
+            <Route path="about" element={<About />} />
+            <Route index element={<Home />} />
+            <Route path="contacts" element={<Contacts />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </div>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,11 +1,42 @@
-import { Link } from 'react-router-dom'
+//добавление ссылок через компонент Link
+/*import { Link } from 'react-router-dom'
 
 function Menu() {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/contacts">Contacts</Link>
-      <Link to="/about">Contacts</Link>
+      <Link to=".">Home</Link>
+      <Link to="contacts">Contacts</Link>
+      <Link to="about">About</Link>
+    </nav>
+  )
+}
+
+export default Menu*/
+
+//Добавление ссылок через компонент NavLink
+
+import { NavLink } from 'react-router-dom'
+
+function Menu() {
+  return (
+    <nav>
+      <NavLink
+        //Добавление класса для стилизации через деструктуризацию
+        className={({ isActive }) => (isActive ? 'activeLink' : 'inactiveLink')}
+        to="."
+        end
+      >
+        Home
+      </NavLink>
+      <NavLink
+        style={({ isActive }) =>
+          isActive ? { color: 'chocolate', textDecoration: 'none' } : {}
+        }
+        to="contacts"
+      >
+        Contacts
+      </NavLink>
+      <NavLink to="about">About</NavLink>
     </nav>
   )
 }
