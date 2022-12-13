6999adf5edaae565dca3a3be9545c469569c98f1
Eugene Mikhailov <svork@ya.ru>
2022-12-13 20:16:34 +0300

    link not exist

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,15 +1,30 @@
-import { Link, useParams } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Link, useNavigate, useParams } from 'react-router-dom'
 import projects from '../data/projects'
+import NotFound from './NotFound'
 
 const Project = () => {
   const params = useParams()
+  //перенаправление
+  const navigate = useNavigate()
   console.log(params)
   const project = projects.find((project) => project.slug === params.slug)
+  //перенапраление на страницу проектов
+
+  useEffect(() => {
+    if (!project) {
+      navigate('..', { relative: 'path' })
+    }
+  }, [project])
+  //отображение страницы нот фаунд без перенаправления
+  /*if (!project) {
+    return <NotFound />
+  }*/
   return (
     <>
-      <h1>{project.title}</h1>
-      <h2>{project.slug}</h2>
-      <h3>id: {project.id}</h3>
+      <h1>{project?.title}</h1>
+      <h2>{project?.slug}</h2>
+      <h3>id: {project?.id}</h3>
 
       {/*
       переход по абсолютной ссылке
